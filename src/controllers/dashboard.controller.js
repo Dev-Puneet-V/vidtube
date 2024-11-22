c85475df2cc0ca968da479693274373ae0eed353
puneet <imhtcy312@gmail.com>
2024-11-22 14:19:25 +0530

    add get channel videos route

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
-import { Video } from "../models/video.models.js";
-// import { Subscription } from "../models/subscription.models.js";
-// import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.models.js";
+import { Video } from "../models/video.models.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
   try {
     const stats = await User.aggregate([
       {
@@ -64,8 +61,12 @@ const getChannelStats = asyncHandler(async (req, res) => {
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
   try {
+    const owner = new mongoose.Types.ObjectId(req.user._id);
+    const channelVideos = await Video.find({
+      owner,
+    });
+    res.status(200).json(new ApiResponse(200, channelVideos, "Videos fetched successfully"));
   } catch (error) {
     throw new ApiError(500, "Error fetching channel video");
   }
